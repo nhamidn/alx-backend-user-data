82576b457410d42f601761e9a5712bae7785893e
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-11 23:59:09 +0000

    initial commit

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+filtred_logger module.
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Function that returns the log message obfuscated."""
+    for field in fields:
+        message = re.sub(f"{field}=.*?{separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
