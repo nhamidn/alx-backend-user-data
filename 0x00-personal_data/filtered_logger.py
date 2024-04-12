252d903e363e64b986ed00ce75b17e57a7a558bd
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-12 01:04:42 +0000

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -52,3 +52,16 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """function that Returns a connector to a MySQL database."""
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+    connector = mysql.connector.connection.MySQLConnection(user=username,
+                                                           password=password,
+                                                           host=host,
+                                                           database=db_name)
+    return connector
