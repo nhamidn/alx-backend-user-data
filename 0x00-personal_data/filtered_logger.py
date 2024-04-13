63a3e913ea216bc93efd6ba04653434ca5505176
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-13 02:39:56 +0000

    pycodestyle fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,12 +14,12 @@ PII_FIELDS = ("name", "email", "ssn", "password", "phone")
 
 
 def filter_datum(fields: List[str], redaction: str,
-                  message: str, separator: str) -> str:
-     """Function that returns the log message obfuscated."""
-     for field in fields:
-         message = re.sub(f"{field}=.*?{separator}",
-                          f"{field}={redaction}{separator}", message)
-     return message
+                 message: str, separator: str) -> str:
+    """Function that returns the log message obfuscated."""
+    for field in fields:
+        message = re.sub(f"{field}=.*?{separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
