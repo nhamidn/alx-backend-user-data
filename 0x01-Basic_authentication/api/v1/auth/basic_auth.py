d6605c41a8e10acd0f2732ac7c24491afe07381b
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-19 00:55:24 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic_auth module.
 """
 
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """returns the decoded value of a Base64 string."""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base64_bytes = base64.b64decode(base64_authorization_header)
+            return base64_bytes.decode('utf-8')
+        except Exception:
+            return None
