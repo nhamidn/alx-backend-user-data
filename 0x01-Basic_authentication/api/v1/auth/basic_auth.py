db861e835ac459ee490483cf3e8a41029d0a74b5
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-19 01:31:26 +0100

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,17 @@ class BasicAuth(Auth):
             return base64_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """returns the user email and password from the Base64 value"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        credentials = decoded_base64_authorization_header.split(':', 1)
+        if len(credentials) == 2:
+            return credentials[0], credentials[1]
+        return None, None
