64c4752a10cd5896176748f02d52d5276c88cb2a
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-19 03:51:44 +0100

    fixe

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,6 +16,8 @@ class Auth:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
+        if path[-1] != '/':
+            path += '/'
         for excluded_path in excluded_paths:
             if fnmatch.fnmatch(path, excluded_path):
                 return False
