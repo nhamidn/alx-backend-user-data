b587531e56a2fa03c091c537f3d65d870f0d8ea9
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-21 02:57:30 +0100

    Logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -35,3 +35,16 @@ class SessionAuth(Auth):
         uid = self.user_id_for_session_id(self.session_cookie(request))
         user = User.get(uid)
         return user
+
+    def destroy_session(self, request=None):
+        """Method that deletes the user session / logout."""
+        if request is None:
+            return False
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return False
+        user_id = self.user_id_for_session_id(session_cookie)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_cookie]
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -32,3 +32,15 @@ def auth_session() -> str:
             resp.set_cookie(s_name, s_id)
             return resp
     return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def handle_logout():
+    """ DELETE /api/v1/auth_session/logout
+    Handle user logout
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
