9187f2a7a89fda256415ec46b5d4ac2155d57243
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-24 12:56:21 +0100

    Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Flask app module."""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "Bienvenue"})
