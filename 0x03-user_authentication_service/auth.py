2be8d6c9e6895f62eec58b4e1c7242c14ea6e823
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-24 02:00:09 +0100

    Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""Auth module."""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Method that hash a passord."""
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
