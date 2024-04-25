199f4195b6f0276b68b678ac8c9e6c4364d3408e
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-25 14:21:22 +0100

    another test

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -12,15 +12,17 @@ AUTH = Auth()
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """end-point to update a user's password."""
-    email = request.form['email']
-    reset_token = request.form['reset_token']
-    new_password = request.form['new_password']
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    is_password_changed = False
     try:
         AUTH.update_password(reset_token, new_password)
-        msg = {"email": email, "message": "Password updated"}
-        return jsonify(msg), 200
+        is_password_changed = True
     except ValueError:
-        return abort(403)
+        is_password_changed = False
+    if not is_password_changed:
+        abort(403)
 
 
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
