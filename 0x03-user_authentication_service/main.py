e43f340e8bfda7af9b03e310bc8f900b833c064f
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-25 16:11:14 +0100

    all remaining tests

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -73,6 +73,32 @@ def log_out(session_id: str) -> None:
     assert response.json() == msg
 
 
+def reset_password_token(email: str) -> str:
+    """Test function for getting reset password token."""
+    data = {
+        "email": email
+    }
+    response = requests.post(f'{BASE_URL}/reset_password', data=data)
+    assert response.status_code == 200
+    reset_token = response.json().get("reset_token")
+    msg = {"email": email, "reset_token": reset_token}
+    assert response.json() == msg
+    return reset_token
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """Test function for resseting password."""
+    data = {
+        "email": email,
+        "reset_token": reset_token,
+        "new_password": new_password
+    }
+    response = requests.put(f'{BASE_URL}/reset_password', data=data)
+    msg = {"email": email, "message": "Password updated"}
+    assert response.status_code == 200
+    assert response.json() == msg
+
+
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
 NEW_PASSWD = "t4rt1fl3tt3"
@@ -86,3 +112,6 @@ if __name__ == "__main__":
     session_id = log_in(EMAIL, PASSWD)
     profile_logged(session_id)
     log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
