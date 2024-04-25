c14ae2d8e5caaaf53b907792cf0708274ddf10d3
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-25 16:03:52 +0100

    test 4 an 5

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -37,6 +37,31 @@ def profile_unlogged() -> None:
     assert response.status_code == 403
 
 
+def log_in(email: str, password: str) -> str:
+    """Test function for succesful login."""
+    data = {
+        "email": email,
+        "password": password
+    }
+    response = requests.post(f'{BASE_URL}/sessions', data=data)
+    msg = {"email": email, "message": "logged in"}
+    assert response.status_code == 200
+    assert response.json() == msg
+    session_id = response.cookies.get("session_id")
+    return session_id
+
+
+def profile_logged(session_id: str) -> None:
+    """Test function  for profile logged in."""
+    cookies = {
+        "session_id": session_id
+    }
+    response = requests.get(f'{BASE_URL}/profile', cookies=cookies)
+    msg = {"email": EMAIL}
+    assert response.status_code == 200
+    assert response.json() == msg
+
+
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
 NEW_PASSWD = "t4rt1fl3tt3"
@@ -47,3 +72,5 @@ if __name__ == "__main__":
     register_user(EMAIL, PASSWD)
     log_in_wrong_password(EMAIL, NEW_PASSWD)
     profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
